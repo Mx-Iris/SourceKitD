54e953ed52cc2f98254ef52e7a707fc1d5490667
Mx-Iris <61279231+Mx-Iris@users.noreply.github.com>
2023-03-26 01:51:39 +0800

    Add SKDDictionary convenient keypath subscript access

--- Sources/SourceKitD/SKDRequestDictionary.swift
+++ Sources/SourceKitD/SKDRequestDictionary.swift
@@ -18,52 +18,93 @@ import CRT
 #endif
 
 public final class SKDRequestDictionary {
-  public let dict: sourcekitd_object_t?
-  public let sourcekitd: SourceKitD
+    public let dict: sourcekitd_object_t?
+    public let sourcekitd: SourceKitD
 
-  public init(_ dict: sourcekitd_object_t? = nil, sourcekitd: SourceKitD) {
-    self.dict = dict ?? sourcekitd.api.request_dictionary_create(nil, nil, 0)
-    self.sourcekitd = sourcekitd
-  }
-
-  deinit {
-    sourcekitd.api.request_release(dict)
-  }
-
-  public subscript(key: sourcekitd_uid_t?) -> String {
-    get { fatalError("request is set-only") }
-    set { sourcekitd.api.request_dictionary_set_string(dict, key, newValue) }
-  }
-  public subscript(key: sourcekitd_uid_t?) -> Int {
-    get { fatalError("request is set-only") }
-    set { sourcekitd.api.request_dictionary_set_int64(dict, key, Int64(newValue)) }
-  }
-  public subscript(key: sourcekitd_uid_t?) -> sourcekitd_uid_t? {
-    get { fatalError("request is set-only") }
-    set { sourcekitd.api.request_dictionary_set_uid(dict, key, newValue) }
-  }
-  public subscript(key: sourcekitd_uid_t?) -> SKDRequestDictionary {
-    get { fatalError("request is set-only") }
-    set { sourcekitd.api.request_dictionary_set_value(dict, key, newValue.dict) }
-  }
-  public subscript<S>(key: sourcekitd_uid_t?) -> S where S: Sequence, S.Element == String {
-    get { fatalError("request is set-only") }
-    set {
-      let array = SKDRequestArray(sourcekitd: sourcekitd)
-      newValue.forEach { array.append($0) }
-      sourcekitd.api.request_dictionary_set_value(dict, key, array.array)
+    public init(_ dict: sourcekitd_object_t? = nil, sourcekitd: SourceKitD) {
+        self.dict = dict ?? sourcekitd.api.request_dictionary_create(nil, nil, 0)
+        self.sourcekitd = sourcekitd
     }
-  }
-  public subscript(key: sourcekitd_uid_t?) -> SKDRequestArray {
-    get { fatalError("request is set-only") }
-    set { sourcekitd.api.request_dictionary_set_value(dict, key, newValue.array) }
-  }
+
+    deinit {
+        sourcekitd.api.request_release(dict)
+    }
+
+    public subscript(key: sourcekitd_uid_t?) -> String {
+        get { fatalError("request is set-only") }
+        set { sourcekitd.api.request_dictionary_set_string(dict, key, newValue) }
+    }
+
+    public subscript(key: sourcekitd_uid_t?) -> Int {
+        get { fatalError("request is set-only") }
+        set { sourcekitd.api.request_dictionary_set_int64(dict, key, Int64(newValue)) }
+    }
+
+    public subscript(key: sourcekitd_uid_t?) -> sourcekitd_uid_t? {
+        get { fatalError("request is set-only") }
+        set { sourcekitd.api.request_dictionary_set_uid(dict, key, newValue) }
+    }
+
+    public subscript(key: sourcekitd_uid_t?) -> SKDRequestDictionary {
+        get { fatalError("request is set-only") }
+        set { sourcekitd.api.request_dictionary_set_value(dict, key, newValue.dict) }
+    }
+
+    public subscript<S>(key: sourcekitd_uid_t?) -> S where S: Sequence, S.Element == String {
+        get { fatalError("request is set-only") }
+        set {
+            let array = SKDRequestArray(sourcekitd: sourcekitd)
+            newValue.forEach { array.append($0) }
+            sourcekitd.api.request_dictionary_set_value(dict, key, array.array)
+        }
+    }
+
+    public subscript(key: sourcekitd_uid_t?) -> SKDRequestArray {
+        get { fatalError("request is set-only") }
+        set { sourcekitd.api.request_dictionary_set_value(dict, key, newValue.array) }
+    }
+
+    @inlinable
+    public subscript(key: KeyPath<sourcekitd_keys, sourcekitd_uid_t>) -> String {
+        get { self[sourcekitd.keys[keyPath: key]] }
+        set { self[sourcekitd.keys[keyPath: key]] = newValue }
+    }
+    
+    @inlinable
+    public subscript(key: KeyPath<sourcekitd_keys, sourcekitd_uid_t>) -> Int {
+        get { self[sourcekitd.keys[keyPath: key]] }
+        set { self[sourcekitd.keys[keyPath: key]] = newValue }
+    }
+
+    @inlinable
+    public subscript(key: KeyPath<sourcekitd_keys, sourcekitd_uid_t>) -> sourcekitd_uid_t? {
+        get { self[sourcekitd.keys[keyPath: key]] }
+        set { self[sourcekitd.keys[keyPath: key]] = newValue }
+    }
+
+    @inlinable
+    public subscript(key: KeyPath<sourcekitd_keys, sourcekitd_uid_t>) -> SKDRequestDictionary {
+        get { self[sourcekitd.keys[keyPath: key]] }
+        set { self[sourcekitd.keys[keyPath: key]] = newValue }
+    }
+
+    @inlinable
+    public subscript<S>(key: KeyPath<sourcekitd_keys, sourcekitd_uid_t>) -> S where S: Sequence, S.Element == String {
+        get { self[sourcekitd.keys[keyPath: key]] }
+        set { self[sourcekitd.keys[keyPath: key]] = newValue }
+    }
+
+    @inlinable
+    public subscript(key: KeyPath<sourcekitd_keys, sourcekitd_uid_t>) -> SKDRequestArray {
+        get { self[sourcekitd.keys[keyPath: key]] }
+        set { self[sourcekitd.keys[keyPath: key]] = newValue }
+    }
 }
 
 extension SKDRequestDictionary: CustomStringConvertible {
-  public var description: String {
-    let ptr = sourcekitd.api.request_description_copy(dict)!
-    defer { free(ptr) }
-    return String(cString: ptr)
-  }
+    public var description: String {
+        let ptr = sourcekitd.api.request_description_copy(dict)!
+        defer { free(ptr) }
+        return String(cString: ptr)
+    }
 }
--- Sources/SourceKitD/SKDResponseDictionary.swift
+++ Sources/SourceKitD/SKDResponseDictionary.swift
@@ -18,52 +18,81 @@ import CRT
 #endif
 
 public final class SKDResponseDictionary {
-  public let dict: sourcekitd_variant_t
-  let resp: SKDResponse
+    public let dict: sourcekitd_variant_t
+    let resp: SKDResponse
 
-  public var sourcekitd: SourceKitD { return resp.sourcekitd }
+    public var sourcekitd: SourceKitD { return resp.sourcekitd }
 
-  public init(_ dict: sourcekitd_variant_t, response: SKDResponse) {
-    self.dict = dict
-    self.resp = response
-  }
+    public init(_ dict: sourcekitd_variant_t, response: SKDResponse) {
+        self.dict = dict
+        self.resp = response
+    }
 
-  public subscript(key: sourcekitd_uid_t?) -> String? {
-    return sourcekitd.api.variant_dictionary_get_string(dict, key).map(String.init(cString:))
-  }
-  public subscript(key: sourcekitd_uid_t?) -> Int? {
-    let value = sourcekitd.api.variant_dictionary_get_value(dict, key)
-    if sourcekitd.api.variant_get_type(value) == SOURCEKITD_VARIANT_TYPE_INT64 {
-      return Int(sourcekitd.api.variant_int64_get_value(value))
-    } else {
-      return nil
+    public subscript(key: sourcekitd_uid_t?) -> String? {
+        return sourcekitd.api.variant_dictionary_get_string(dict, key).map(String.init(cString:))
     }
-  }
-  public subscript(key: sourcekitd_uid_t?) -> Bool? {
-    let value = sourcekitd.api.variant_dictionary_get_value(dict, key)
-    if sourcekitd.api.variant_get_type(value) == SOURCEKITD_VARIANT_TYPE_BOOL {
-      return sourcekitd.api.variant_bool_get_value(value)
-    } else {
-      return nil
+
+    public subscript(key: sourcekitd_uid_t?) -> Int? {
+        let value = sourcekitd.api.variant_dictionary_get_value(dict, key)
+        if sourcekitd.api.variant_get_type(value) == SOURCEKITD_VARIANT_TYPE_INT64 {
+            return Int(sourcekitd.api.variant_int64_get_value(value))
+        } else {
+            return nil
+        }
     }
-  }
-  public subscript(key: sourcekitd_uid_t?) -> sourcekitd_uid_t? {
-    return sourcekitd.api.variant_dictionary_get_uid(dict, key)
-  }
-  public subscript(key: sourcekitd_uid_t?) -> SKDResponseArray? {
-    let value = sourcekitd.api.variant_dictionary_get_value(dict, key)
-    if sourcekitd.api.variant_get_type(value) == SOURCEKITD_VARIANT_TYPE_ARRAY {
-      return SKDResponseArray(value, response: resp)
-    } else {
-      return nil
+
+    public subscript(key: sourcekitd_uid_t?) -> Bool? {
+        let value = sourcekitd.api.variant_dictionary_get_value(dict, key)
+        if sourcekitd.api.variant_get_type(value) == SOURCEKITD_VARIANT_TYPE_BOOL {
+            return sourcekitd.api.variant_bool_get_value(value)
+        } else {
+            return nil
+        }
     }
-  }
+
+    public subscript(key: sourcekitd_uid_t?) -> sourcekitd_uid_t? {
+        return sourcekitd.api.variant_dictionary_get_uid(dict, key)
+    }
+
+    public subscript(key: sourcekitd_uid_t?) -> SKDResponseArray? {
+        let value = sourcekitd.api.variant_dictionary_get_value(dict, key)
+        if sourcekitd.api.variant_get_type(value) == SOURCEKITD_VARIANT_TYPE_ARRAY {
+            return SKDResponseArray(value, response: resp)
+        } else {
+            return nil
+        }
+    }
+    
+    @inlinable
+    public subscript(key: KeyPath<sourcekitd_keys, sourcekitd_uid_t>) -> String? {
+        self[sourcekitd.keys[keyPath: key]]
+    }
+
+    @inlinable
+    public subscript(key: KeyPath<sourcekitd_keys, sourcekitd_uid_t>) -> Int? {
+        self[sourcekitd.keys[keyPath: key]]
+    }
+    
+    @inlinable
+    public subscript(key: KeyPath<sourcekitd_keys, sourcekitd_uid_t>) -> Bool? {
+        self[sourcekitd.keys[keyPath: key]]
+    }
+
+    @inlinable
+    public subscript(key: KeyPath<sourcekitd_keys, sourcekitd_uid_t>) -> sourcekitd_uid_t? {
+        self[sourcekitd.keys[keyPath: key]]
+    }
+
+    @inlinable
+    public subscript(key: KeyPath<sourcekitd_keys, sourcekitd_uid_t>) -> SKDResponseArray? {
+        self[sourcekitd.keys[keyPath: key]]
+    }
 }
 
 extension SKDResponseDictionary: CustomStringConvertible {
-  public var description: String {
-    let ptr = sourcekitd.api.variant_description_copy(dict)!
-    defer { free(ptr) }
-    return String(cString: ptr)
-  }
+    public var description: String {
+        let ptr = sourcekitd.api.variant_description_copy(dict)!
+        defer { free(ptr) }
+        return String(cString: ptr)
+    }
 }
